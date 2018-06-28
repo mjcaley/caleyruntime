b74f90c3ae48b73c3b9bee656f0b5c2914cee5f1
Michael Caley <mjcaley@darkarctic.com>
2018-06-28 09:32:47 -0400

    Fixed pointer math

--- src/gc.c
+++ src/gc.c
@@ -6,17 +6,28 @@
 int gc_mark = 0;
 
 
+static inline int* gc_get_mark_ptr(void* ptr)
+{
+    return (int*)(ptr - sizeof(int));
+}
+
+static inline Type** gc_get_type_ptr(void* ptr)
+{
+    return (Type**)(ptr - sizeof(int) - sizeof(Type*));
+}
+
 void gc_init(const Type* type, void* ptr)
 {
-    *(int*)(ptr - sizeof(int)) = gc_mark;
-    *(Type**)(ptr - sizeof(int) - sizeof(Type*)) = type;
+    *gc_get_mark_ptr(ptr) = gc_mark;
+    *gc_get_type_ptr(ptr) = type;
 }
 
 void* gc_malloc(const Type* type)
 {
-    void* ptr = malloc(sizeof(Type*) + sizeof(int) + type->size);
+    void* alloc = malloc(sizeof(Type*) + sizeof(int) + type->size);
+    void* ptr = (alloc + sizeof(Type*) + sizeof(int));
     gc_init(type, ptr);
-    return (char*)ptr + sizeof(Type*) + sizeof(int);
+    return ptr;
 }
 
 void gc_free(void* ptr)
@@ -26,19 +37,18 @@ void gc_free(void* ptr)
 
 void gc_mark_ptr(void* ptr)
 {
-    *(int*)(ptr - sizeof(int)) = gc_mark;
+    *gc_get_mark_ptr(ptr) = gc_mark;
 }
 
 
 int gc_get_mark(void* ptr)
 {
-    return *(int*)(ptr - sizeof(int));
+    return *gc_get_mark_ptr(ptr);
 }
 
 
 void traverse(void* ptr, void (*f)(void*))
 {
-    Type* temp = (Type*)((char*)ptr - sizeof(int) - sizeof(Type*));
+    Type* temp = *gc_get_type_ptr(ptr);
     temp->traverse(ptr, f);
 }
-
