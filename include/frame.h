87a50905e2f4d70915193ffdb4586b171817237f
Michael Caley <mjcaley@darkarctic.com>
2018-06-29 15:02:00 -0400

    Work on frame

--- include/frame.h
+++ include/frame.h
@@ -3,16 +3,11 @@
 #include <stddef.h>
 
 
-typedef struct Frame
-{
-    size_t num;
-    void* locals[];
-} Frame;
-
 typedef struct FrameNode FrameNode;
 struct FrameNode
 {
-    Frame* frame;
+    size_t num;
+    void* locals[];
     FrameNode* next;
 };
 
@@ -20,3 +15,18 @@ typedef struct FrameList
 {
     FrameNode* head;
 } FrameList;
+
+
+void frame_push(FrameList* stack, FrameNode* frame)
+{
+    frame->next = stack->head;
+    head = frame;
+}
+
+
+void frame_pop(FrameList* stack)
+{
+    FrameNode* head = stack->head;
+    free(stack->head);
+    stack->head = head;
+}
